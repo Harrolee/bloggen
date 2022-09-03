c02323417ee6526d635d6f8ae0d534b4f779b201
harrolee <halzinnia@gmail.com>
2022-09-03 12:40:33 -0400

    Add config commands

--- pypi-package/src/bloggen.py
+++ pypi-package/src/bloggen.py
@@ -1,12 +1,15 @@
-from config import configure_bloggen
+from config import Configure
 from static_site import Site 
 from dotenv import load_dotenv
 load_dotenv()
+import sys
 import argparse
     
 def main():
-    configure_bloggen()
+    config = Configure()
     parser = create_parser()
+    if len(sys.argv) == 1:
+        parser.print_help()
     args = parser.parse_args()
     site = Site()
     if args.add:
@@ -21,11 +24,25 @@ def main():
             site.publish()
         else:
             site.publish(args.publish)
+    elif args.config:  
+        if args.config in ['no_args','create','new']:
+            config.create_config()
+        elif args.config in ['list','ls']:
+            config.list_config_names()
+        else:
+            if config.config_exists(args.config):
+                print(f"Activating {args.config}")
+                config.set_active_config(args.config)
+            else:
+                print(f"Config {args.config} does not exist.")
+                print(f"These configs exist:")
+                config.list_config_names()
 
 def create_parser():
     parser = argparse.ArgumentParser(description="Create a static site!")
     parser.add_argument('-a', '--add', help="Upload a .md file.")
     parser.add_argument('--destroy', help="Terminates bucket!")
+    parser.add_argument('-c','--config', help="Activate or create a configuration", nargs='?', const='no_args')
     parser.add_argument('-g','--generate', help="Builds static site locally.", nargs='?', const='no_args')
     parser.add_argument('--remove', help="Removes a file from bucket.")
     parser.add_argument('--sync', help="Uploads directory to bucket.")
