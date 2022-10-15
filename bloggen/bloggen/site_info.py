365a261a77d3d22768637ffb52204fea059822af
harrolee <halzinnia@gmail.com>
2022-10-15 13:30:12 -0400

    Use id for nodes in index object, rather than dir name

--- bloggen/bloggen/site_info.py
+++ bloggen/bloggen/site_info.py
@@ -6,7 +6,7 @@ import uuid
 
 directive_symbol_start = '<!--$'
 directive_symbol_end = '$-->'
-
+timestamp_format = '%m-%d-%YT%H:%M:%S'
 class Site_Info:
 
     def __init__(self, target_dir, user:str):
@@ -19,13 +19,21 @@ class Site_Info:
         self.supported_directives = [{'name':'subjects', 'function': self.extract_subjects}, {'name':'tags', 'function': self.extract_tags}, {'name':'style', 'function': self.extract_style}]
         self.get_site_info()
 
+    def generate_ids(self, paths: List, id_tag: str, username: str, return_mapping: bool=False) -> List[str]:
+        ids_paths: List[Dict[str,str]] = [{ 'id': self.generate_id(username, id_tag), 'path': path } for path in paths]
+        if return_mapping:
+            return [ele['id'] for ele in ids_paths], ids_paths
+        return [ele['id'] for ele in ids_paths]
+
     def get_site_info(self) -> dict():
+        node_paths = self.__get_sub_directories(self.target_dir)
+        node_ids = self.generate_ids(node_paths, 'blog', self.user)
+        
         note_paths = self.__get_filepaths(self.target_dir)
-        noteIds_notepaths: List[Dict[str,str]] = [{ 'id': self.generate_id(self.user, "note"), 'path': path } for path in note_paths]
-        note_ids = [ele['id'] for ele in noteIds_notepaths]
+        note_ids, noteIds_notepaths = self.generate_ids(note_paths, 'note', self.user, True)
         
         # index object
-        self.__create_index(self.target_dir, note_ids)
+        self.__create_index(self.target_dir, node_ids, note_ids)
         # TODO rootnode and nodes list should contain node ids, not folder names
         print(self.index)
 
@@ -37,19 +45,17 @@ class Site_Info:
         self.create_data(self.user, self.index, noteIds_notepaths)
         print(self.data)
 
-    def __create_index(self, target_dir, note_ids: List[str]):
+    def __create_index(self, target_dir, node_ids, note_ids: List[str]):
         """
         index is a list of contents.
         index is not a record of relationships.
         """
         # root node is the highest dir
         rootNode = PurePath(target_dir).name
-        # `nodes` contains list the dirs present in the dir heirarchy
-        nodes = self.__get_sub_directories(target_dir)
 
         # filepaths = self.__get_filepaths(target_dir)
         self.index.update({"rootNode":rootNode})
-        self.index.update({"nodes":nodes})
+        self.index.update({"nodes":node_ids})
         self.index.update({"notes":note_ids})
 
     def find_relationships(self):
@@ -76,8 +82,6 @@ class Site_Info:
         id = noteId_notepath['id']
         path = noteId_notepath['path']
         directive_data = self.extract_data(path)
-        print(f'directive_data is: {directive_data}')
-        # TODO account for directive data being empty AKA user did not write a directive for tags or for subjects
         return {
             'id': id,
             'content': path,
@@ -90,20 +94,17 @@ class Site_Info:
         """
         As you decide to add more metadata objects, return more objects from here
         """
-        return {'timestamp': datetime.now().strftime("%m/%d/%Y %H:%M:%S")}
+        return {'timestamp': datetime.now().strftime(timestamp_format)}
     
     def note_metadata(self, path):
         """
         As you decide to add more metadata objects, return more objects from here
         """
         style = 'default' # TODO extract style from the file. It is a directive. use extract_style()
-        return {'timestamp': datetime.now().strftime("%Y/%m/%dT%H:%M:%S")}, {'style':style}
+        return {'timestamp': datetime.now().strftime(timestamp_format),'style':style}
 
     def __get_sub_directories(self, dir_name) -> list[str]:
         sub_dirs = [f.name for f in os.scandir(dir_name) if f.is_dir()]
-        # what was I after with the below code?
-        # for dir_name in sub_dirs:
-        #     sub_dirs.extend(self.__get_sub_directories(dir_name))
         return sub_dirs
     
     def __get_filepaths(self, dir_name) -> list[str]:
@@ -119,21 +120,19 @@ class Site_Info:
         directive_data: Dict[str: [str]] = self.get_directives(contents, filepath)
         return directive_data
         # TODO add support for directives that are commands
-        #return self.execute_commands(directives)
+        # return self.execute_commands(directive_data)
 
     def get_directives(self, contents: str, filepath: str) -> Dict[str, List[str]]:
         """
         At present, directive commands do not have context. They are simply pieces of text that are ignored by html
         """
-        directives = {}
         supported_directives = [s['name'] for s in self.supported_directives]
+        directives = {directive:set() for directive in supported_directives}
         substr_start = 0
 
         directive_index = contents.find(directive_symbol_start, substr_start)
-        print(f'directive_index: {directive_index}')
         while directive_index != -1:    
             colon_index = contents.find(':', directive_index)
-            print(f'colon_index: {colon_index}')
             directive_end_index = contents.find(directive_symbol_end, colon_index)
             directive_start = directive_index + len(directive_symbol_start)
             directive: str = contents[directive_start:colon_index].strip(' ').lower()
@@ -152,11 +151,10 @@ class Site_Info:
 
     def execute_commands(self, found_directives: Dict[str, List[str]]):
         results = {}
+        self.supported_directives
         for supported_directive in self.supported_directives: # [{'name':'subjects', 'function': self.extract_subjects}, {'name':'tags', 'function': self.extract_tags}]
-            print(f'found these directives: {found_directives}')
             if supported_directive['name'] in found_directives:
                 commands = found_directives[supported_directive['name']]
-                print(f'commands are: {commands}')
                 for command in commands:
                     results[supported_directive['name']] = supported_directive['function'](command)
         return results
